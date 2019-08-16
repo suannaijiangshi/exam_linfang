e497a15fd38adfc03df43b5822cdbbfc0d4566ec
suannaijiangshi <53719049+suannaijiangshi@users.noreply.github.com>
2019-08-16 10:58:43 +0800

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,34 @@
+
+
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+
+#esp.osdebug(None)
+
+import gc
+
+import os
+import time
+#import webrepl
+
+#webrepl.start()
+
+gc.collect()
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('ZTE-221', '0123456789@jiaxing')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+    
+do_connect()
+time.sleep(3)
+
+
+
+
